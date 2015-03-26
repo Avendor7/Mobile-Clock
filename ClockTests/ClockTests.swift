d51513937abcb6fb6ba524377b5a836b90108137
200220207 <200220207@ba-lc5-22.student.georgianc.on.ca>
2015-03-26 14:35:40 -0400

    Initial Commit

---
+++ ClockTests/ClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  ClockTests.swift
+//  ClockTests
+//
+//  Created by 200220207 on 2015-03-26.
+//  Copyright (c) 2015 Stephen Squared. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
